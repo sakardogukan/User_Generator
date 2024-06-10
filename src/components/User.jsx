995e209354a7029990c373167b2b5b6408bf878e
DGKN <sakardogukan@gmail.com>
2024-06-10 22:49:22 +0300

    User.jsx file updated

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react"
+import Header from "./Header"
 import Container from "react-bootstrap/Container"
+import UserStyle from "../scss/user.module.scss"
 
 const User = () => {
   const [userData, setUserData] = useState()
@@ -19,17 +21,27 @@ const User = () => {
 
   return (
     <div className="text-center">
-      <h1 className="text-info mt-3 mb-5">User Random Generator App</h1>
-      <Container className="border border-4 w-50 p-3">
-        <img src={userData?.picture?.large} alt="" className="rounded-circle mt-3" />
-        <h2>Hi, My name is</h2>
-        <h1>{userData?.name?.first} {userData?.name?.last}</h1>
-        <h2>{new Date(userData?.dob?.date).toLocaleDateString("tr-TR")}</h2>
-        <h3>{userData?.email}</h3>
-        <h4>{userData?.phone}</h4>
-        <h5>{userData?.location?.city} / {userData?.location?.country}</h5>
+      <h1 className="text-info mt-5 mb-5 fs-2">User Random Generator App</h1>
+      <Container className="border border-4 border-dark w-50 p-3 text-center">
+        <div className={UserStyle.specialTextUser}>
+        <img
+          src={userData?.picture?.large}
+          alt=""
+          className="rounded-circle mt-3 mb-3"
+        />
+        </div>
+        <h2 className={UserStyle.specialTextUser}>
+          Hi, My name is{" "}
+          <span className={UserStyle["span"]}>
+            "{userData?.name?.first} {userData?.name?.last}"
+          </span>
+        </h2>
+        <h5 className={UserStyle.specialTextUser} style={{ color: "blue" }}>
+          You can see my information from the icons...
+        </h5>
+        <Header data={userData} />
       </Container>
-      <button className="btn btn-success mt-5" onClick={getUser}>
+      <button className="btn btn-success mt-5 fs-3" onClick={getUser}>
         Click for Random User Generate
       </button>
     </div>
